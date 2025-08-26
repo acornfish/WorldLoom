7466d0f78c42dd3a39cfcb65788641849504913b
acornfish <usingnot07@gmail.com>
2025-08-26 11:17:47 +0300

    map pins

--- static/maps/app.js
+++ static/maps/app.js
@@ -22,6 +22,7 @@ import "../libs/dist/jstree.js"
 var isMapListMenuActive = false
 var currentImage = ""
 var pins = []
+var movingPin = ""
 
 function setMapListButtonEvent() {
     $(".map-list-ul button:not(.add-new-map)").on("click", (event) => {
@@ -116,14 +117,39 @@ function selectMap(){
             currentImage = data.image || ""
             $("#map-name-field").val(data.name)
             pins = data.pins
+            placePins()
             drawMap(currentImage)
         }).catch(data => {
             currentImage = ""
             $("#map-name-field").val("")
             pins = []
+            placePins()
             drawMap(currentImage) 
         })
     }    
+}
+
+function placePins (){
+    $(".pin").remove()
+    for(let pin of pins){
+        let newPin = $(`<div class="pin" uid=${pin.uid}><i class="fa-solid fa-map-pin"></i></div>`)
+
+        newPin.attr("style", `
+            top: ${pin.top}px;
+            left: ${pin.left}px;
+        `)   
+        newPin.on("mousedown", (event) => {
+            if(event.button == 0){ 
+                event.currentTarget.classList.add("dragging-pin") 
+                pinPositionData.lastLeft = event.clientX
+                pinPositionData.lastTop = event.clientY
+                pinPositionData.totalTop = (event.currentTarget.getBoundingClientRect().top) || 0
+                pinPositionData.totalLeft =  (event.currentTarget.getBoundingClientRect().left) || 0
+            } 
+        })
+    
+        $(".map-container").append(newPin)
+    }
 }
 
 retrieveMapList().then(maps => {
@@ -207,19 +233,86 @@ const mapPositionData = {
     isDragging: false
 }
 
+const pinPositionData = {
+    lastLeft: 0,
+    lastTop: 0,
+    totalLeft: 0,
+    totalTop: 0,
+}
+
+//control pins 
+$(document).on("contextmenu", (event) => {
+    event.preventDefault()
+
+    let newPinUid = window.uid()
+    let newPin = $(`<div class="pin" uid=${newPinUid}><i class="fa-solid fa-map-pin"></i></div>`)
+    pins.push({
+        uid: newPinUid,
+        top: event.clientY,
+        left: event.clientX,
+        name: "", 
+        description: ""
+    })
+
+    newPin.attr("style", `
+        top: ${event.clientY}px;
+        left: ${event.clientX}px;
+    `)   
+    newPin.on("mousedown", (event) => {
+        if(event.button == 0){ 
+            event.currentTarget.classList.add("dragging-pin") 
+            pinPositionData.lastLeft = event.clientX
+            pinPositionData.lastTop = event.clientY
+            pinPositionData.totalTop = (event.currentTarget.getBoundingClientRect().top) || 0
+            pinPositionData.totalLeft =  (event.currentTarget.getBoundingClientRect().left) || 0
+        } 
+    })
+
+    $(".map-container").append(newPin)
+})
+
 //make map draggable
 $(".map-render-image").on("mousedown", (event) => {
-    let mapElem = $(".map-render-image")
-    mapPositionData.lastLeft = event.clientX
-    mapPositionData.lastTop = event.clientY
-    mapPositionData.isDragging = true;
+    if(event.button == 0){
+        mapPositionData.lastLeft = event.clientX
+        mapPositionData.lastTop = event.clientY
+        mapPositionData.isDragging = true;    
+    }
 })
 
 $(document).on("mousemove", (event) => {
     let mapElem = $(".map-render-image")
-    let diffX = (event.clientX - mapPositionData.lastLeft)
-    let diffY = (event.clientY - mapPositionData.lastTop)
+
+    if($(".dragging-pin").length){
+        let diffX = (event.clientX - pinPositionData.lastLeft)
+        let diffY = (event.clientY - pinPositionData.lastTop)
+
+        pinPositionData.lastLeft = event.clientX
+        pinPositionData.lastTop = event.clientY
+
+        pinPositionData.totalLeft += diffX
+        pinPositionData.totalTop += diffY
+
+        $(".dragging-pin").attr("style", `
+            top: ${pinPositionData.totalTop}px;
+            left: ${pinPositionData.totalLeft}px;
+        `)
+
+        let rectP = $(".dragging-pin").get(0).getBoundingClientRect()
+        let rectM = mapElem.get(0).getBoundingClientRect()
+       
+        let currentPinIndex = pins.findIndex(pin => pin.uid == $($(".dragging-pin")).attr("uid"))
+        pins[currentPinIndex].left = rectP.left - rectM.left
+        pins[currentPinIndex].top = rectP.top - rectM.top
+
+        mapPositionData.isDragging = false
+    }
+
+
     if (mapPositionData.isDragging) {
+        let diffX = (event.clientX - mapPositionData.lastLeft)
+        let diffY = (event.clientY - mapPositionData.lastTop)
+   
         mapPositionData.lastLeft = event.clientX
         mapPositionData.lastTop = event.clientY
 
@@ -230,11 +323,28 @@ $(document).on("mousemove", (event) => {
             top: ${mapPositionData.totalTop}px;
             left: ${mapPositionData.totalLeft}px;
         `)
+
+        $(".pin").each((index, el) => {
+            let rectP = el.getBoundingClientRect()
+
+            $(el).attr("style", `
+                top: ${diffY+ rectP.top}px;
+                left: ${diffX + rectP.left}px;
+            `)
+            
+            let currentPinIndex = pins.findIndex(pin => pin.uid == $(el).attr("uid"))
+            pins[currentPinIndex].left = rectP.left - rectM.left
+            pins[currentPinIndex].top = rectP.top - rectM.top
+        })
     }
+
+
 })
 
 $(document).on("mouseup", (event) => {
     mapPositionData.isDragging = false
+    const draggingPin = $(".dragging-pin")
+    draggingPin.removeClass("dragging-pin")
 })
 
 $("#map-image-field-button").on("click", (event) => {
